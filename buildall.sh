e395769daba8d08fe758c4727e50620824563296
Zheng Chen @home <zhchen@cuc.edu.cn>
2015-12-26 20:03:15 +0800

    增加了修改对比文件

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-g++ snake.cpp -o output/snake.exe
-g++ gzh3.cpp -o output/gzh3.exe
-g++ double.cpp -o output/double.exe
+
+g++ changes.cpp -o output/changes.exe
+
+#g++ snake.cpp -o output/snake.exe
+#g++ gzh3.cpp -o output/gzh3.exe
+#g++ double.cpp -o output/double.exe
