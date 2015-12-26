e1f2ddf94e534a3b724358381865a05c004f4391
Zheng Chen @home <zhchen@cuc.edu.cn>
2015-12-26 22:09:03 +0800

    提交代码

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-g++ changes.cpp -o output/changes.exe
+#g++ changes.cpp -o output/changes.exe
 
-#g++ snake.cpp -o output/snake.exe
+g++ snake.cpp -o output/snake.exe
 #g++ gzh3.cpp -o output/gzh3.exe
 #g++ double.cpp -o output/double.exe
